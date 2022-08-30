ab10ecc8a9660ff66d123b3ee20696067ad8d2ee
potemkin010 <zjakkl25aa@gmail.com>
2022-08-30 19:00:06 +0900

    new file:   brainwaves_client.py

---
+++ brainwaves_client.py
@@ -0,0 +1,57 @@
+import atexit
+import numpy as np
+import socket
+import sys
+import time
+import argparse
+
+# socket
+client = None
+
+#コマンドライン引数受取
+parser = argparse.ArgumentParser(description = 'add two integer')
+parser.add_argument('--id',type=float,default=1,help='student id')
+parser.add_argument('--sleep',type=float,default=1,help='sleep or awake')
+parser.add_argument('--host',type=str,default='localhost',help='host')
+parser.add_argument('--port',type=int,help='port')
+
+args = parser.parse_args()
+
+# socket切断
+def disconnect():
+    global client
+    if client:
+        client.close()
+    print("atexit function called")
+
+# socket接続
+def connect(host, port):
+    global client
+    try:
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client.connect((host, port))
+        # 接続に成功したらsocketを返す
+        return client
+    except Exception as err:
+        print(err)
+        # 接続に失敗したらNoneを返す
+        return None
+
+def main():
+    global client
+    # localhost:33400に接続
+    client = connect(args.host,args.port)
+    if not client:
+        # 接続に失敗したらプログラムを終了する
+        sys.exit()
+    while True:
+        client.sendall(np.array([args.id, 1, 1, args.sleep]))
+        # 1s待機
+        time.sleep(1)
+
+# プログラム終了時にdisconnect関数を実行する
+atexit.register(disconnect)
+
+# main関数をentry pointとして実行する
+if __name__ == "__main__":
+    main()
